02c091ded78480c8e93c40c5449b43667ef4e05d
Roberto Sartori <roberto.sartori.android@gmail.com>
2021-05-31 20:48:18 +0200

    Android.mk: remove references to Sony devices
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,6 +1,4 @@
 ifneq ($(filter $(QCOM_NEW_MEDIA_PLATFORM), $(TARGET_BOARD_PLATFORM)),)
-ifneq ($(filter 4.14, $(SOMC_KERNEL_VERSION)),)
-
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
@@ -16,7 +14,6 @@ include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 include $(QCOM_MEDIA_ROOT)/libarbitrarybytes/Android.mk
 ifeq ($(ENABLE_HYP),true)
 include $(QCOM_MEDIA_ROOT)/hypv-intercept/Android.mk
-endif
 endif
 endif
 endif
